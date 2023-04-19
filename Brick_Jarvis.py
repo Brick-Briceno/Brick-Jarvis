335676a85852da38b4fac26cfad6b6d6a000eba3
Miguel Briceño <48538138+Brick-Briceno@users.noreply.github.com>
2023-04-19 09:37:12 -0400

    Update Brick_Jarvis.py

--- Brick_Jarvis.py
+++ Brick_Jarvis.py
@@ -11,7 +11,7 @@ pip install openai
 pip install gTTS
 """
 
-def hablar(texto):#
+def hablar(texto):
     dialogo = gTTS(text = texto, lang = "es-us", slow = False)
     dialogo.save("dialogo.mp3")
     repr = vlc.MediaPlayer("dialogo.mp3")
@@ -19,15 +19,14 @@ def hablar(texto):#
     #repr.stop() #sirve para pausar la reproducción xd
 
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 def pensar(pregunta): #esto genera el texto
     completion = openai.Completion.create(engine="text-davinci-003", prompt=pregunta, max_tokens=4000)
     return completion.choices[0].text
 
-
 hablar("Hola! Bienvenido!!!")
-
+ 
 print("""Bienvenido!!!
 
 Soy una interfaz de preguntas creada por @Brick_Briceno ¿en que puedo ayudarte? :D
@@ -37,11 +36,12 @@ Trucos:
 para Salir solo escriba "salir"
 para Repetir la respuesta solo escriba "repite"
 para Copiar la respuesta al portapepeles solo escriba "copiar"
+para Activar o Decativar la Voz solo escriba "voz"
 
 
 """)
 
-
+voz = True
 
 while True:
     pregunta = input("Tu: ")
@@ -56,10 +56,16 @@ while True:
     
     elif pregunta == "copiar":
         Copiar(respuesta)
+        print("Copiado!")
+
+    elif pregunta == "voz":
+        voz = not(voz)
+
     else:
         try:
             respuesta = pensar(pregunta)
-            hablar(respuesta)
+            if voz:
+                hablar(respuesta)
             print("IA:" + respuesta + "\n")
         except:
-            print("\nOh hubo un error :(\nReconectando...")#esto por si hay un error en el servidor
+            print("\nOh hubo un error :(\nIntente una vez más! :D")#esto por si hay un error en el servidor
